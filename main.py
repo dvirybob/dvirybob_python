02f0290c64ea027cf2da6e26ba605e1ccb5ee357
dvirybob <dvirybob@gmail.com>
2023-05-17 07:27:33 +0300

    first try

---
+++ main.py
@@ -0,0 +1,13 @@
+a = "hi dad"
+print(a)
+b = range(101)
+for number in b:
+    print(number)
+    c = number % 10
+    if c == 0:
+        print("ten")
+    else:
+        print("not ten")
+        c = 10
+
+c = c +1
